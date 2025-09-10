c1bec29816cea79aefada7753e02ca0f1f8e5641
IvorGradiškiZrinski <ivor.gradiski.zrinski@deepit.hr>
2025-09-10 07:35:02 +0200

    init projects

---
+++ issuing/src/main/java/com/example/card/issuing/CardIssuingApplication.java
@@ -0,0 +1,13 @@
+package com.example.card.issuing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardIssuingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardIssuingApplication.class, args);
+	}
+
+}
---
+++ issuing/src/test/java/com/example/card/issuing/CardIssuingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.card.issuing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardIssuingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
